298b4933fa9ce9fc052cbe30db8a7bff52f28580
Resham <resha@Mac.attlocal.net>
2025-10-05 19:29:04 -0700

    Added Node to the Front of the List

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@ struct Node
 {
     double rating;
     string comment;
+    Node *next;
 };
 
 int main()
 {
     int userChoice = 0;
+    Node *head = nullptr;
+    char yesOrNo;
 
     cout << "Which linked list method should we use?";
     cout << "[1] New nodes are added at the head of the linked list";
@@ -23,13 +26,47 @@ int main()
         Node *temp = new Node;
 
         cout << "Enter review rating 0-5: ";
-        cin >> temp.rating;
+        cin >> temp->rating;
 
         cout << "Enter review comments: ";
-        cin >> temp.comment;
+        cin >> temp->comment;
+
+        if (head == nullptr)
+        {
+            head = temp;
+        }
+        else 
+        {
+            if (userChoice == 1)
+            {
+                // Add to the front
+                head = temp;
+                temp->next = head;
+            }
+            else 
+            {
+                // Add to the tail
+            }
+        }
+
+        cout << "Enter another review? Y/N: ";
+        cin >> yesOrNo;
+
+        if (yesOrNo == 'N' || yesOrNo == 'n')
+        {
+            break;
+        }
     }
 
     cout << "Outputting all reviews:" << endl;
+
+    Node *current = head;
+
+    while (current != nullptr)
+    {
+        cout << "Rating: " << current->rating << ": Comment: " << current->comment << endl;
+        current = current->next;
+    }
 
     return 0;
 }
